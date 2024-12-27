9e8aaf705f9f4dc76ce8ae529552d2de292458b0
ASHISH KUMAR <152021705+LEYTHAN-X@users.noreply.github.com>
2024-12-27 12:08:35 +0530

    Add Backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderpaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
+import '../data/backend-practise.js';
 
 renderpaymentSummary();
 renderOrderSummary();
